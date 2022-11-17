23e7654ae3051ebe5eca53a455041849d5c522b4
CharlesHuaringa <charles.huaringa@gmail.com>
2022-11-17 17:44:15 -0500

    Ejercicios js S16

---
+++ ejercicios.js
@@ -0,0 +1,75 @@
+
+// let a = +prompt("Ingrese un número")
+// let b = +prompt("Ingrese otro número")
+
+
+// let resta = function(a,b){
+//     return a - b
+// }
+
+// let result2 = resta(a,b);
+// alert(`El resultado de la resta es ${result2}`)
+
+// let multipli = function(x,y){
+//     return x * y
+// }
+
+
+// let expont = function(x,y){
+//     return Math.pow(x,y);
+// }
+
+// let suma = (x,y) => {return x+y};
+
+
+// let result = multipli(5,2);
+// alert(result);
+
+// result = suma(3,4);
+// alert(result);
+
+
+
+
+// let gradoscelcius = +prompt("Ingrese una temperatura en celcius")
+// function convertircelciusaFarenheit(gradoscelcius){
+//     let resultado = gradoscelcius * 1.8 + 32;
+//     return resultado
+// }
+
+// let resultado = convertircelciusaFarenheit(gradoscelcius)
+// alert(`${gradoscelcius} °C = ${resultado} °F`)
+
+
+
+
+// let gradosFarenheit = +prompt("Ingrese una temperatura en Farenhei")
+// function convertirFarenheiaCelcius(gradosFarenheit){
+//     let resultado2 = (gradosFarenheit - 32) * 5 /9;
+//     return resultado2
+// }
+
+// let resultado2 = convertirFarenheiaCelcius(gradosFarenheit)
+// alert(`${gradosFarenheit} °F = ${resultado2} °C`)
+
+
+
+
+
+
+
+let numero = +prompt("Ingrese el primer número")
+let numero2 = +prompt("Ingrese el segundo número")
+
+let suma = function(numero,numero2){
+    return numero + numero2
+}
+
+let resultado = suma(numero, numero2)
+alert(`La suma de dichos números es ${resultado}`)
+
+
+
+
+
+
